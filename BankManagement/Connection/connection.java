f658117deed2432bc139fbd7c681b106af1167e2
Ritik Maheshwari <maheshwariritik85@gmail.com>
2025-05-25 17:06:12 +0530

    Initial commit for banking system using JDBC

---
+++ BankManagement/Connection/connection.java
@@ -0,0 +1,26 @@
+package BankManagement.Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+// Global connection Class
+public class connection {
+	static Connection con; // Global Connection Object
+
+	public static Connection getConnection() {
+		try {
+
+			String mysqlJDBCDriver = "com.mysql.cj.jdbc.Driver"; // jdbc driver
+			String url = "jdbc:mysql://localhost:3306/BankApp"; // mysql url
+			String user = "ritik"; // mysql username
+			String pass = "Ritik@123"; // mysql passcode
+			Class.forName(mysqlJDBCDriver);
+			con = DriverManager.getConnection(url, user,
+					pass);
+		} catch (Exception e) {
+			System.out.println("Connection Failed!");
+		}
+
+		return con;
+	}
+}
